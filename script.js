2af2a44044398d3beca3e95ca54779e943aafb6c
detectiveseo <detectiveseo1@gmai.com>
2023-09-30 18:59:20 +0600

    feature(drag-drop): function has been added

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
+const listBag = document.querySelectorAll(".container");
 const dragableItems = document.querySelectorAll(".draggable");
 
 dragableItems.forEach((item) => {
     item.addEventListener("dragstart", (e) => {
-        setTimeout(() => item.classList.add("draging"), 1)
-        
-    })
+        const draggingItem = e.target;
+        setTimeout(() => draggingItem.classList.add("dragging"), 0);
+    });
 
+    item.addEventListener("dragend", (e) => {
+        const draggingItem = e.target;
+        draggingItem.classList.remove("dragging");
+    });
+});
+  
+listBag.forEach((bag) => {
+    bag.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        const draggingItem = document.querySelector(".dragging");
+        const siblings = [...bag.querySelectorAll(".draggable:not(.dragging)")];
+        console.log(siblings);
 
-// remove class name from drag item 
-    item.addEventListener("dragend", () =>  item.classList.remove("draging"))
-})
+        let nextSibling = siblings.find((sibling) => {
+            return e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2;
+        });
+
+        bag.insertBefore(draggingItem, nextSibling);
+    });
+});
